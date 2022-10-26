17211bdee747a33f6bca6158114b2273a6d82b41
tal101d <116706412+tal101d@users.noreply.github.com>
2022-10-26 22:53:32 +0300

    Update Assignment1.java

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -3,6 +3,6 @@
 public class Assignment1 {
 	public static void main(String[] args){
 		// uncomment the following line to print your output:
-		//System.out.println("New char is " + newChar + ".");
+		System.out.println("New char is " + newChar + ".");
 	}
 }
