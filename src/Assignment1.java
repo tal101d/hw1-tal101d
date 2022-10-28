a41ebf79ec6d51773943cb5a6cce64d5e241c9a4
tal101d <116706412+tal101d@users.noreply.github.com>
2022-10-28 07:47:38 +0300

    Update Assignment1.java

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -4,7 +4,7 @@ public class Assignment1 {
 	public static void main(String[] args){
 		int offset = (int)args[0];
 		char chr = (int)args[1];
-		newChar = (char)(chr + offset))
+		newChar = (char)(chr + offset));
 		System.out.println("New char is " + newChar + ".");
 	}
 }
