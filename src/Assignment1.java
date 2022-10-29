65b81a7d35a39f35084d78282587d5eadae586f7
tal101d <116706412+tal101d@users.noreply.github.com>
2022-10-29 12:41:28 +0300

    Update Assignment1.java

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,9 +1,12 @@
 
 
 public class Assignment1 {
-	 // Getting arguments
-        int offset =Integer.parseInt(args[0]);
-        char chr = args[1].charAt(0);
+	public static void main(String[] args){
+
+        // Getting arguments
+
+        char chr = args[0].charAt(0);
+        int offset =Integer.parseInt(args[1]);
 
         //Calculating the new Char
         char newChar = (char)((int)(chr) + offset);
